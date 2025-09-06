7b64b2400cca672158d89c2f4902f8d76c3e4b36
kubilay türe <kubilayture@gmail.com>
2025-09-06 21:30:05 +0300

    fix: update image source path in LoginForm and SignupForm components

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -155,7 +155,7 @@ export function LoginForm({
 
           <div className="bg-muted relative hidden md:block">
             <img
-              src="public/placeholder.svg"
+              src="/placeholder.svg"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -172,7 +172,7 @@ export function SignupForm({
 
           <div className="bg-muted relative hidden md:block">
             <img
-              src="public/placeholder.svg"
+              src="/placeholder.svg"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
